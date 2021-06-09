13ed1d624707a033576ed5d230aeb5d1b2791cab
robertinglin <robertinglin@gmail.com>
2021-06-08 21:04:09 -0400

    Load Gist into App - Add public gist loading - Add indentation for FunctionDeclaration and Program

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,59 @@ import Program from './nodes/Program';
 const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
 import { ProgramNode } from "../types/ast";
+import { getGist, Gist } from "../services/github";
+import { parseCodeStringToProgramNode } from '../utils/parser';
 
-class App extends React.Component<Record<string, unknown>, undefined> {
+class App extends React.Component<Record<string, unknown>, { programNode : ProgramNode | null, errorMessage: string }> {
+  state = {
+    programNode: null ,
+    errorMessage: ''
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.loadGist();
+  }
+
+  async loadGist() {
+    try {
+      const gist : Gist = await getGist('24753b4fce0c340531ea64fd4e9c8ef5');
+      console.log(gist.content);
+      const parsedGist : ProgramNode = parseCodeStringToProgramNode(gist.content);
+      this.setState({
+        programNode: parsedGist
+      });
+    } catch (rawError) {
+      
+
+    console.log(rawError);
+
+      const error = rawError as Error;
+      this.setState({
+        errorMessage: error.message
+      });
+    }
+  }
+
   public render() {
-    let progNode = ast as  unknown as ProgramNode;
+    if (this.state.errorMessage !== '') {
+      return <div>
+        <h1 style={{ color: 'red' }}>
+          Oh no there's an Error
+        </h1>
+        <p>{this.state.errorMessage}</p>
+      </div>
+    }
+
+    if (this.state.programNode == null) {
+      return <div className="app">
+        Loading
+      </div>
+    }
     return (
       <div className="app">
-        <Program node={progNode} />
+        <Program node={this.state.programNode} />
       </div>
     );
   }
--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,6 +2,6 @@ import { ProgramNode } from "../types/ast";
 import * as acorn from 'acorn';
 
 
-export const parse = (input : string) : ProgramNode => {
+export const parseCodeStringToProgramNode = (input : string) : ProgramNode => {
     return (acorn.parse(input, {ecmaVersion: 5}) as unknown) as ProgramNode;
 }
