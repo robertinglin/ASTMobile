4613934b3ae80e5f4fbd21339836183528e38e4b
Shivam Patel <patelsd96@gmail.com>
2021-06-10 00:18:24 -0400

    using astNode2 which is new gist for NodeSwitch

--- src/components/nodes/NodeSwitch.tsx
+++ src/components/nodes/NodeSwitch.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
-import { AstNode, FunctionDeclaration, Identifier, ReturnStatement, BinaryExpression } from "../../types/ast";
+import { AstNode, FunctionDeclaration, Identifier, ReturnStatement, BinaryExpression, CallExpression, Literal } from "../../types/ast";
 import { leftCurlyBrace, rightCurlyBrace }  from "../../utils/curlyBraces";
 import FunctionDeclarationRenderer from './functions/FunctionDeclaration';
 type NodeSwitchProps = {
@@ -9,6 +9,8 @@ type NodeSwitchProps = {
 
 const NodeSwitch = (props: NodeSwitchProps) => {
         
+        console.log("props.node.type",props.node.type)
+        console.log('props.node', props.node);
     switch (props.node.type) {
         case 'FunctionDeclaration':
             const functionNode : FunctionDeclaration = props.node as FunctionDeclaration;
@@ -25,6 +27,15 @@ const NodeSwitch = (props: NodeSwitchProps) => {
         case 'BinaryExpression':
             const binaryExpressionNode : BinaryExpression = props.node as BinaryExpression;
             return <span><NodeSwitch node = {binaryExpressionNode.left}/> {binaryExpressionNode.operator.toString()} <NodeSwitch node={binaryExpressionNode.right}/> </span>
+
+        case 'CallExpression':
+            const callExpressionNode : CallExpression = props.node as CallExpression;
+            return <span><NodeSwitch node = {callExpressionNode.callee}/> ({callExpressionNode.arguments.map((node) => <NodeSwitch node={node} />)}) </span>
+
+        case 'Literal':
+            const literalNode : Literal = props.node as Literal;
+            return <span>{literalNode.value}</span>
+
 
         default: 
             return <div>Unknown {props.node.type}</div>
